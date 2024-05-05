6dbc88caabee58a51bc881487ba66969a940107e
microwavestine <microwavestine@gmail.com>
2024-05-05 23:18:51 +0900

    bugfix: ledButtons undefined error in controltower, and images not showing up in farm diary

--- hanginggarden/hanginggarden/settings.py
+++ hanginggarden/hanginggarden/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'controltower/media/'
+MEDIA_URL = 'media/'
 
 
 # Quick-start development settings - unsuitable for production
